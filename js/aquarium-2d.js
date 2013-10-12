23dc41a3ebedece747c4806016b5e76c6e7bc4e8
Jan Horava <czechinthemail@gmail.com>
2013-10-12 12:12:29 -0700

    added random fish speed, improved formatting

--- js/aquarium-2d.js
+++ js/aquarium-2d.js
@@ -1,111 +1,114 @@
 function draw(){
-	if(key_left){
-		camera_x -= 5;
-	}
-	if(key_right){
-		camera_x += 5;
-	}
-	if(key_down){
-		camera_y += 5;
-	}
-	if(key_up){
-		camera_y -= 5;
-	}
+    if(key_left){
+        camera_x -= 5;
+    }
+    if(key_right){
+        camera_x += 5;
+    }
+    if(key_down){
+        camera_y += 5;
+    }
+    if(key_up){
+        camera_y -= 5;
+    }
 
-	buffer.clearRect(
-	    0,
-	    0,
-	    width,
-	    height
-	);
+    buffer.clearRect(
+        0,
+        0,
+        width,
+        height
+    );
 
-	i = fish.length - 1;
-	if(i >= 0){
-		buffer.fillStyle = '#888';
+    i = fish.length - 1;
+    if(i >= 0){
+        buffer.fillStyle = '#888';
 
         // set position now to simplify fish placement math
-		buffer.translate(
-		    x - camera_x,
-		    y - camera_y
-		);
+        buffer.translate(
+            x - camera_x,
+            y - camera_y
+        );
 
-		do{
-		    // fish move in the direction they are facing
-			fish[i][0] -= fish[i][2];
+        do{
+            // fish move in the direction they are facing
+            fish[i][0] -= fish[i][2];
 
             // if a fish travels past the edge of the screen, swap it to the other edge
-			if(fish[i][0] > camera_x + x + 50 || fish[i][0] < camera_x - x - 50){
-				fish[i][0] = camera_x + (fish[i][0] > camera_x + x ? -x - 50 : x + 50);
+            if(fish[i][0] > camera_x + x + 50 || fish[i][0] < camera_x - x - 50){
+                fish[i][0] = camera_x + (fish[i][0] > camera_x + x ? -x - 50 : x + 50);
 
-				// randomize fish y position
-				fish[i][1] = camera_y + random_number(height) - y;
-			}
+                // randomize fish y position
+                fish[i][1] = camera_y + random_number(height) - y;
+            }
 
-			// draw fish
-			buffer.beginPath();
-			buffer.moveTo(
-			    fish[i][0],
-			    fish[i][1]
-			);
-			buffer.lineTo(
-			    fish[i][0] + 15 * fish[i][2],
-			    fish[i][1] - 10
-			);
-			buffer.lineTo(
-			    fish[i][0] + 45 * fish[i][2],
-			    fish[i][1] + 10
-			);
-			buffer.lineTo(
-			    fish[i][0] + 45 * fish[i][2],
-			    fish[i][1] - 10
-			);
-			buffer.lineTo(
-			    fish[i][0] + 15 * fish[i][2],
-			    fish[i][1] + 10
-			);
-			buffer.closePath();
-			buffer.fill();
-		}while(i--);
+            // determine movement direction based on speed
+            var dir = fish[i][2] > 0 ? 1 : -1;
+
+            // draw fish
+            buffer.beginPath();
+            buffer.moveTo(
+                fish[i][0],
+                fish[i][1]
+            );
+            buffer.lineTo(
+                fish[i][0] + 15 * dir,
+                fish[i][1] - 10
+            );
+            buffer.lineTo(
+                fish[i][0] + 45 * dir,
+                fish[i][1] + 10
+            );
+            buffer.lineTo(
+                fish[i][0] + 45 * dir,
+                fish[i][1] - 10
+            );
+            buffer.lineTo(
+                fish[i][0] + 15 * dir,
+                fish[i][1] + 10
+            );
+            buffer.closePath();
+            buffer.fill();
+        }while(i--);
 
         // reset fish placement
-		buffer.translate(
-		    camera_x - x,
-		    camera_y - y
-		)
-	}
+        buffer.translate(
+            camera_x - x,
+            camera_y - y
+        )
+    }
 
-	canvas.clearRect(
-	    0,
-	    0,
-	    width,
-	    height
-	);
-	canvas.drawImage(
-	    get('buffer'),
-	    0,
-	    0
-	);
+    canvas.clearRect(
+        0,
+        0,
+        width,
+        height
+    );
+    canvas.drawImage(
+        get('buffer'),
+        0,
+        0
+    );
 }
 
 function get(i){
-	return document.getElementById(i)
+    return document.getElementById(i)
 }
 
 function random_number(i){
-	return Math.floor(Math.random() * i)
+    return Math.floor(Math.random() * i)
 }
 
 function resize(){
-	width = window.innerWidth;
-	get('buffer').width = width;
-	get('canvas').width = width;
+    width = window.innerWidth;
+    get('buffer').width = width;
+    get('canvas').width = width;
 
-	height = window.innerHeight;
-	get('buffer').height = height;
-	get('canvas').height = height;
+    height = window.innerHeight;
+    get('buffer').height = height;
+    get('canvas').height = height;
 
-	x = width / 2;
-	y = height / 2;
+    x = width / 2;
+    y = height / 2;
 }
 
 var buffer = get('buffer').getContext('2d');
@@ -128,11 +131,11 @@ resize();
 
 // create 10 randomly placed fish
 do{
-	fish.push([
-		random_number(width) - x,// x
-		random_number(height) - y,// y
-		[-1,1][random_number(2)]// facing direction
-	]);
+    fish.push([
+        random_number(width) - x,// x
+        random_number(height) - y,// y
+        Math.random() * 10 - 5// movement speed
+    ]);
 }while(i--);
 
 setInterval(
@@ -143,41 +146,41 @@ setInterval(
 window.onresize = resize;
 
 window.onkeydown = function(e){
-	key = window.event ? event : e;
-	key = key.charCode ? key.charCode : key.keyCode;
+    key = window.event ? event : e;
+    key = key.charCode ? key.charCode : key.keyCode;
 
-	if(key == 65){// A
-		key_left = 1;
+    if(key == 65){// A
+        key_left = 1;
 
-	}else if(key == 68){// D
-		key_right = 1;
+    }else if(key == 68){// D
+        key_right = 1;
 
-	}else if(key == 83){// S
-		key_down = 1;
+    }else if(key == 83){// S
+        key_down = 1;
 
-	}else if(key == 87){// W
-		key_up = 1;
+    }else if(key == 87){// W
+        key_up = 1;
 
-	}else if(key == 72){// H
-		camera_x = 0;
-		camera_y = 0;
-	}
+    }else if(key == 72){// H
+        camera_x = 0;
+        camera_y = 0;
+    }
 };
 
 window.onkeyup = function(e){
-	key = window.event ? event : e;
-	key = key.charCode ? key.charCode : key.keyCode;
+    key = window.event ? event : e;
+    key = key.charCode ? key.charCode : key.keyCode;
 
-	if(key == 65){// A
-		key_left = 0;
+    if(key == 65){// A
+        key_left = 0;
 
-	}else if(key == 68){// D
-		key_right = 0;
+    }else if(key == 68){// D
+        key_right = 0;
 
-	}else if(key == 83){// S
-		key_down = 0;
+    }else if(key == 83){// S
+        key_down = 0;
 
-	}else if(key == 87){// W
-		key_up = 0;
-	}
+    }else if(key == 87){// W
+        key_up = 0;
+    }
 };
